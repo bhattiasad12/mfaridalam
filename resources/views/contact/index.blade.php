af01b78fb2931c2c81b07c4442403d98dbb3c80d
bhattiasad12 <bhattiasad12@gmail.com>
2025-01-10 00:41:38 +0500

    contact me

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -81,8 +81,8 @@
                                                     role="button">Message</span><span class="dt-column-order"></span></th>
                                             <th class="min-w-125px dt-orderable-asc dt-orderable-desc" data-dt-column="3"
                                                 rowspan="1" colspan="1" aria-label="Role: Activate to sort"
-                                                tabindex="0"><span class="dt-column-title"
-                                                    role="button">Date</span><span class="dt-column-order"></span></th>
+                                                tabindex="0"><span class="dt-column-title" role="button">Date</span><span
+                                                    class="dt-column-order"></span></th>
                                             <th class="text-end min-w-100px dt-orderable-none" data-dt-column="4"
                                                 rowspan="1" colspan="1" aria-label="Actions"><span
                                                     class="dt-column-title">Actions</span><span
@@ -120,18 +120,29 @@
                                                 </td>
                                                 <td>
                                                     <div class="d-flex flex-column">
-                                                        <span class="text-gray-800 mb-1">{{ date('d-m-Y H:i', strtotime($value->created_at)); }}</span>
+                                                        <span
+                                                            class="text-gray-800 mb-1">{{ date('d-m-Y H:i', strtotime($value->created_at)) }}</span>
                                                     </div>
                                                 </td>
 
                                                 <td class="text-end">
-                                                    <form action="{{ route('contact.destroy', $value->id) }}" method="POST"
-                                                        style="display:inline;">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit"
-                                                            class="menu-link px-3 btn btn-danger text-white">Delete</button>
-                                                    </form>
+                                                    <a href="#"
+                                                        class="btn btn-light btn-active-light-primary btn-flex btn-center btn-sm"
+                                                        data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+                                                        Actions
+                                                        <i class="ki-duotone ki-down fs-5 ms-1"></i> </a>
+                                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4"
+                                                        data-kt-menu="true">
+                                                        <div class="menu-item px-3">
+                                                            <form action="{{ route('contact.destroy', $value->id) }}"
+                                                                method="POST" style="display:inline;">
+                                                                @csrf
+                                                                @method('DELETE')
+                                                                <button type="submit"
+                                                                    class="menu-link px-3 btn btn-danger text-white">Delete</button>
+                                                            </form>
+                                                        </div>
+                                                    </div>
                                                 </td>
                                             </tr>
                                         @endforeach
